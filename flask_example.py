b29eee4463844a692d78e64e237cad4621c6e0b4
anuragpatle <anurag.ap6@gmail.com>
2022-01-10 20:16:24 +0530

    made facial recog. more efficient

--- flask_example.py
+++ flask_example.py
@@ -4,11 +4,13 @@ from werkzeug.utils import secure_filename
 from flask_session import Session
 from deepface import DeepFace
 from pathlib import Path
+from deepface.basemodels import VGGFace
 
 
 UPLOAD_FOLDER_FOR_RECOG = 'static/uploaded_files_for_recog'
 DATASET_FOLDER = 'dataset'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+model = VGGFace.loadModel()
 
 app = Flask(__name__)
 app.secret_key = "secret key"
@@ -56,12 +58,20 @@ def upload_image():
         flash('Image successfully uploaded and displayed below')
         img_path = UPLOAD_FOLDER_FOR_RECOG  + '/' + filename
         print('upload_image filename: ' + filename)
-        obj = DeepFace.analyze(img_path =img_path, actions=['age', 'gender', 'race', 'emotion'], enforce_detection=False)
-        print("obj: ", obj)
-        df = DeepFace.find(img_path = img_path, db_path = "./dataset1", enforce_detection=False)
-        print(df.head())
+        
+        df = DeepFace.find(img_path = img_path, db_path = "./dataset", model_name = 'VGG-Face', model = model, distance_metric = 'cosine', enforce_detection=False)
+        # print(df.head())
+        if (df.shape[0] > 0):
+            print("df.iloc[0]['VGG-Face_cosine'] -> ", df.iloc[0]['VGG-Face_cosine'])
+            if (df.iloc[0]['VGG-Face_cosine'] < 0.15):
+                obj = DeepFace.analyze(img_path =img_path, actions=['age', 'gender', 'race', 'emotion'], enforce_detection=False)
+                print("obj: ", obj['dominant_emotion'])
+                print("identity: " , df.iloc[0].identity.split("\\")[1].split("/")[0])
+
+
         # return render_template('upload.html', filename=filename)
-        return "success"
+        res = str(df.head())
+        return res
     else:
         flash('Allowed image types are -> png, jpg, jpeg, gif')
         return redirect(request.url)
