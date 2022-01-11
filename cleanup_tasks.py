96685ed63c88869c1d32c479503fe2c3bee8607c
anuragpatle <anurag.ap6@gmail.com>
2022-01-12 00:20:37 +0530

    some housekeeping

--- cleanup_tasks.py
+++ cleanup_tasks.py
@@ -2,27 +2,35 @@ from pathlib import Path
 import schedule
 import time
 
-def delete_files_by_path():
-    for f in Path('./static/uploaded_files_for_recog').glob('*.jpg'):
-        try:
-            f.unlink()
-            print("Cleaned the directory -> ./static/uploaded_files_for_recog")
-        except OSError as e:
-            print("Error: %s : %s" % (f, e.strerror))
+class Cleanup:
+        
+    def delete_files_by_path(self):
+        for f in Path('./static/uploaded_files_for_recog').glob('*.jpg'):
+            try:
+                f.unlink()
+                print("Cleaned the directory -> ./static/uploaded_files_for_recog")
+            except OSError as e:
+                print("Error: %s : %s" % (f, e.strerror))
 
 
-def cleanup_running_tick():
-    print("Cleanup is running ..")
-  
-schedule.every(2).seconds.do(cleanup_running_tick)
+    def cleanup_running_tick(self):
+        print("Cleanup is running ..")
+    
+    def trigger(self):
+        schedule.every(2).seconds.do(self.cleanup_running_tick)
 
-schedule.every(5).minutes.do(delete_files_by_path)
+        schedule.every(5).minutes.do(self.delete_files_by_path)
 
-# Loop so that the scheduling task
-# keeps on running all time.
-while True:
-  
-    # Checks whether a scheduled task 
-    # is pending to run or not
-    schedule.run_pending()
-    time.sleep(1)
+        # Loop so that the scheduling task
+        # keeps on running all time.
+        while True:
+        
+            # Checks whether a scheduled task 
+            # is pending to run or not
+            schedule.run_pending()
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    obj = Cleanup()
+    obj.trigger()   
--- main.py
+++ main.py
@@ -6,12 +6,19 @@ from deepface import DeepFace
 from pathlib import Path
 from deepface.basemodels import VGGFace
 from pathlib import Path
+import json
+import requests
 
 
 UPLOAD_FOLDER_FOR_RECOG = 'static/uploaded_files_for_recog'
 DATASET_FOLDER = 'dataset'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 model = VGGFace.loadModel()
+
+# Rest api end points
+ROOT_SENTI_API_URI = "http://localhost:5000/facial-senti-api"
+RAW_SENTI_API_URI = ROOT_SENTI_API_URI + "/raw_sentiments"
+headers_ = {'Content-Type': 'application/json'}
 
 app = Flask(__name__)
 app.secret_key = "secret key"
@@ -44,9 +51,7 @@ def upload_image():
     img_capture_time = request.form.get('current_time')
     img_capture_date = request.form.get('current_date')
     img_capture_datetime = request.form.get('current_datetime')
-    print("img_capture_time", img_capture_time)
-    print("img_capture_date", img_capture_date)
-    print("img_capture_datetime", img_capture_datetime)
+    
     img_capturing_device = request.form.get('device_id')
     print("img_capturing_device", img_capturing_device)
     if file.filename == '':
@@ -58,16 +63,58 @@ def upload_image():
         #print('upload_image filename: ' + filename)
         flash('Image successfully uploaded and displayed below')
         img_path = UPLOAD_FOLDER_FOR_RECOG  + '/' + filename
-        print('upload_image filename: ' + filename)
+        # print('upload_image filename: ' + filename)
         
         df = DeepFace.find(img_path = img_path, db_path = "./dataset", model_name = 'VGG-Face', model = model, distance_metric = 'cosine', enforce_detection=False)
         # print(df.head())
         if (df.shape[0] > 0):
             print("df.iloc[0]['VGG-Face_cosine'] -> ", df.iloc[0]['VGG-Face_cosine'])
-            if (df.iloc[0]['VGG-Face_cosine'] < 0.18):
+            if (df.iloc[0]['VGG-Face_cosine'] < 0.15):
                 obj = DeepFace.analyze(img_path =img_path, actions=['age', 'gender', 'race', 'emotion'], enforce_detection=False)
-                print("obj: ", obj['dominant_emotion'])
-                print("identity: " , df.iloc[0].identity.split("\\")[1].split("/")[0])
+                emotion_dict = obj['emotion']
+                # print("emotion_dict: ", emotion_dict)
+                
+                identity = df.iloc[0].identity.split("\\")[1].split("/")[0]
+                # print("identity: " , identity)
+                json_predsDict = obj['emotion']
+                emp_name_n_id = identity.split("-",1) # setting the maxsplit parameter to 1, will return a list with 2 elements!
+
+                sorted_emotion_dict = dict(sorted(emotion_dict.items(), key = lambda x: x[1], reverse=True)) 
+                # print("sorted_emotion_dict: ", sorted_emotion_dict)
+                
+                list_sorted_emotion_dict = list(sorted_emotion_dict)
+                
+                # Note: order of elif block is important here
+                if list_sorted_emotion_dict[0] == "happy":
+                    label = "LIKELY_HAPPY"
+                elif (list_sorted_emotion_dict[0] == "neutral") and (list_sorted_emotion_dict[1] == "happy"):
+                    label = "LIKELY_HAPPY"
+                elif list_sorted_emotion_dict[0] == "surprise" and list_sorted_emotion_dict[1] == "happy":
+                    label = "LIKELY_HAPPY" 
+                elif list_sorted_emotion_dict[0] == "neutral" and list_sorted_emotion_dict[5] == "happy":
+                    label = "LIKELY_NEUTRAL"
+                else:
+                    label = "LIKELY_NOT_HAPPY"
+                    
+                try:
+                    # predsDict_ = json.dumps(json_predsDict, indent=4)
+                    data_ = {
+                        "emotion_scores": json_predsDict,
+                        "date": img_capture_date,
+                        "time": img_capture_time,
+                        "emp_name": emp_name_n_id[0],
+                        "emp_id": emp_name_n_id[1],
+                        "overall_sentiment": label,
+                        "device_id": img_capturing_device
+                    }
+
+                    data_ = json.dumps(data_, indent=4)
+
+                    print ("data: ", data_)
+                    r = requests.post(url = RAW_SENTI_API_URI, data = data_, headers = headers_)
+                    print ("Return of post request", r.json())
+                except Exception as e:
+                    print ("Problem while making post request to url ", RAW_SENTI_API_URI, ", Problem: ", e)
 
 
         # return render_template('upload.html', filename=filename)
@@ -122,6 +169,7 @@ def display_image(filename):
 
 
 if __name__ == '__main__':
+
     app.debug = True
     # 0.0.0.0 means “all IPv4 addresses on the local machine”
     app.run(host='0.0.0.0', port=8080, debug=True)
@@ -131,3 +179,5 @@ if __name__ == '__main__':
     # to manage the secret key!
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
+    
+    
